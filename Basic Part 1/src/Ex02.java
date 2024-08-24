e1763fc0cf7b8ca03643785bd2ab1790fb5544e2
Achala Punsara <achala.magallagoda@gmail.com>
2024-08-24 08:36:02 +0530

    Exercise 2

---
+++ Basic Part 1/src/Ex02.java
@@ -0,0 +1,9 @@
+public class Ex02 {
+    public static void main(String [] args){
+        int x = 74;
+        int y = 36;
+
+        int answer = x+ y;
+        System.out.println(answer);
+    }
+}
