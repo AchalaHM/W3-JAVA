f1e27bb0842b56ff1a5a894daf30bc1264f5de9c
Achala Punsara <achala.magallagoda@gmail.com>
2024-08-24 08:40:45 +0530

    Exercise 3

---
+++ Basic Part 1/src/Ex03.java
@@ -0,0 +1,15 @@
+/*3. Write a Java program to divide two numbers and print them on the screen.
+Test Data :
+50/3
+Expected Output :
+16*/
+
+public class Ex03 {
+
+    public static void main(String [] args){
+        int x= 50;
+        int y = 3;
+
+        System.out.println(x/y);
+    }
+}
